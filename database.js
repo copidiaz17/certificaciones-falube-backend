3d268ecb2fccd7cec426738c2922f2e51c5d223f
copidiaz17 <copidiaz17@gmail.com>
2026-02-02 23:53:01 -0300

    cambios antes de produccion

--- database.js
+++ database.js
@@ -3,28 +3,18 @@ import { Sequelize, DataTypes } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
-const isProd = process.env.NODE_ENV === "production";
-
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT || 3306),
+    port: Number(process.env.DB_PORT || 3306),   // ✅ Aiven usa puerto distinto
     dialect: "mysql",
     logging: false,
-
-    ...(isProd
-      ? {
-          dialectOptions: {
-            ssl: {
-              require: true,
-              rejectUnauthorized: false,
-            },
-          },
-        }
-      : {}),
+    dialectOptions: process.env.DB_SSL === "true"
+      ? { ssl: { rejectUnauthorized: false } }   // ✅ SSL para Aiven/Render
+      : {},
   }
 );
 
