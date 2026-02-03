b9d7d71f2d0b7bab88acc40960fbae6239908eb0
copidiaz17 <copidiaz17@gmail.com>
2026-02-03 02:23:34 -0300

    cambio en modelo Usuario

--- models/Usuario.js
+++ models/Usuario.js
@@ -5,12 +5,12 @@ const Usuario = sequelize.define("Usuario", {
     nombre: { type: DataTypes.STRING, allowNull: false },
     email: { type: DataTypes.STRING, allowNull: false, unique: true },
     password: { type: DataTypes.STRING, allowNull: false },
-    rol: { type: DataTypes.STRING, allowNull: false, defaultValue: "usuario" }
+    rol: { type: DataTypes.STRING, allowNull: false, defaultValue: "usuario" },
 
     {
     tableName: "usuarios",   // 🔥 CLAVE
     freezeTableName: true,   // 🔥 evita pluralización
-  }
+  },
 });
 
 export default Usuario;
